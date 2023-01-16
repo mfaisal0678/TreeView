72934343541149609e7d8594ce7ccc88458bdbc4
Muhammad Faisal Sharif <91064560+mfaisal0678@users.noreply.github.com>
2023-01-16 12:31:36 +0500

    Add files via upload

---
+++ TreeView.ps1
@@ -0,0 +1,17 @@
+﻿$folder = 'C:\Program Files (x86)'
+$size = 0
+$decimalPlaces = 2
+
+$output = @()
+
+Get-ChildItem -Directory $folder | ForEach-Object {
+    $size += (Get-ChildItem -Recurse $_.FullName | Measure-Object -Property Length -Sum).Sum
+    $output += New-Object PSObject -Property @{
+        Folder = $_.FullName
+        Size = "{0:N$decimalPlaces} MB" -f ((Get-ChildItem -Recurse $_.FullName | Measure-Object -Property Length -Sum).Sum / 1MB)
+    }
+}
+
+$output | Out-GridView #Format-Table -AutoSize
+
+"Total size: {0:N$decimalPlaces} MB" -f ($size / 1MB)
